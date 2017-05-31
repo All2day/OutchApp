d9546840c57f59439ecdf0d83908b6d3f02ac8bb
Mads Kessel <mads@all2day.dk>
2017-05-31 11:21:45 +0200

    first commit

---
+++ gameServer.js
@@ -0,0 +1,95 @@
+var http = require('http')
+var url = require('url')
+var fs = require('fs')
+const concat = require('concat-stream');
+var path = require('path')
+var baseDirectory = __dirname   // or whatever base directory you want
+
+var port = 9615
+
+var r = 0;
+var players = {};
+var bombs = {};
+var uid = 0;
+
+http.createServer(function (request, response) {
+   try {
+     var t = new Date().getTime();
+     r++;
+        var requestUrl = url.parse(request.url,true)
+        console.log(requestUrl);
+        var json_data = decodeURI(requestUrl.search.substr(1));
+        var data = JSON.parse(json_data);
+        console.log(data);
+        var UUID = data.UUID;
+        if(!players[UUID]){
+                players[UUID] = {};
+        }
+        var player = players[UUID];
+
+        player.pos = data['pos'];
+        var cmdQueue = null;
+        if(cmdQueue = data['cmdQueue']){
+                for(var i=0;i< cmdQueue.length;i++){
+                        console.log('got cmd'+cmdQueue);
+                        switch(cmdQueue[i][0]){
+                                case 'createBomb':
+                                        var bomb_uid = uid++;
+                                        var duration = 3000;
+                                        bombs[bomb_uid] = {
+                                          owner:UUID,
+                                          start_pos:player.pos,
+                                          end_pos:cmdQueue[i][2],
+                                          time:cmdQueue[i][1],
+                                          duration: duration
+                                  };
+
+                                  setTimeout(function(){
+                                          //go through the players to find if anyone is hit
+                                          Object.keys(players).forEach(function(key){
+                                                  var p = players[key];
+                                                  //calculate the distance
+                                                  var d_x =p.pos[0]-bombs[bomb_uid].end_pos[0];
+                                                  var d_y = p.pos[1]-bombs[bomb_uid].end_pos[1];
+
+                                                  var d = Math.sqrt(d_x*d_x+d_y*d_y);
+                                                  if(d <= 15){
+                                                          console.log('player hit, d:'+d);
+                                                          if(!p.cmdQueue){
+                                                                  p.cmdQueue = [];
+                                                          }
+                                                          p.cmdQueue.push(['die',bombs[bomb_uid].time]);
+                                                  }
+
+                                          });
+                                          delete(bombs[bomb_uid]);
+                                  },duration);
+                                  break;
+                    }
+            }
+    }
+
+    /*concat(request, function(buffer){
+    const data = JSON.parse(buffer.toString());
+    console.log('Data: ', data);
+    });*/
+
+    //console.log(r);
+    response.setHeader('Access-Control-Allow-Origin','*');
+    response.setHeader('Content-Type','application/json');
+    response.writeHead(200);
+    var response_data = {players:players,bombs:bombs,t:t,rt:new Date().getTime() - t};
+    if(player.cmdQueue){
+            response_data.cmdQueue = player.cmdQueue;
+            delete(player.cmdQueue);
+    }
+    response.write(JSON.stringify(response_data));
+    response.end();
+} catch(e) {
+ response.writeHead(500)
+ response.end()     // end the response so browsers don't hang
+ console.log(e.stack)
+}
+}).listen(port)
+
+console.log("listening on port "+port)
